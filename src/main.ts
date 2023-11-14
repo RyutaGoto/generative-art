93c856be209d7e3ebae2ce712a13142bec1c8ee6
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-11-14 23:28:55 +0900

    move circle to center

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const sketch = (p: p5) => {
   /** 初期化処理 */
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.circle(0, 0, 200);
+    p.circle(p.width / 2, p.height / 2, 200);
   };
 
   /** フレームごとの描画処理 */
