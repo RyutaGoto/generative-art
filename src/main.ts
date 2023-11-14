7ae7b9cf0c3dd1b9b34e36a2dc4a0ea35a20acc6
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-11-14 23:55:13 +0900

    draw another circle

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ const sketch = (p: p5) => {
   /** 初期化処理 */
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+
     p.strokeWeight(10);
-    p.stroke(100);
+    p.stroke(240);
+    p.noFill();
+    p.circle(p.width / 4, p.height / 2, 100);
+
+    p.noStroke();
     p.fill(240);
-    p.circle(p.width / 2, p.height / 2, 200);
+    p.circle((p.width / 4) * 3, p.height / 2, 100);
   };
 
   /** フレームごとの描画処理 */
