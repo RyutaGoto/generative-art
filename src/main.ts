2dc09ec6be0a5d09770f8424150a54a20e0b2150
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-11-14 23:34:47 +0900

    change stroke and fill

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ const sketch = (p: p5) => {
   /** 初期化処理 */
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.strokeWeight(10);
+    p.stroke(100);
+    p.fill(240);
     p.circle(p.width / 2, p.height / 2, 200);
   };
 
