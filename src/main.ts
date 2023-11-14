9c1ae7a35f99ee841341519e6289e979e66be57e
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-11-15 00:01:21 +0900

    use mouse point

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,12 @@ const sketch = (p: p5) => {
   /** 初期化処理 */
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-
-    p.strokeWeight(10);
-    p.stroke(240);
-    p.noFill();
-    p.circle(p.width / 4, p.height / 2, 100);
-
-    p.noStroke();
-    p.fill(240);
-    p.circle((p.width / 4) * 3, p.height / 2, 100);
   };
 
   /** フレームごとの描画処理 */
-  p.draw = () => {};
+  p.draw = () => {
+    p.circle(p.mouseX, p.mouseY, 100);
+  };
 };
 
 new p5(sketch);
