8d88d6c0c4527e35e0d61e2d8b1586733612ec06
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-11-17 07:43:54 +0900

    change diameter

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import p5 from "p5";
 
 const sketch = (p: p5) => {
   /** 初期化処理 */
+  let d = 0;
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.noFill();
+    p.noStroke();
+    p.fill(200);
   };
 
   /** フレームごとの描画処理 */
   p.draw = () => {
-    p.stroke(p.random(180, 255));
-    p.ellipse(p.width / 2, p.height / 2, p.random(50, 300), p.random(50, 300));
+    d++;
+    p.clear(0, 0, 0, 0);
+    p.circle(p.width / 2, p.height / 2, d);
   };
 };
 
