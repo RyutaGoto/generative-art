d271ff4aee87cebd19676c80f47c5f4587ab77d9
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-11-18 21:37:35 +0900

    use bgcolor

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,29 @@ import p5 from "p5";
 const sketch = (p: p5) => {
   /** 初期化処理 */
   let d = 0;
+  let bgColor: number, circleColor: number;
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.noStroke();
-    p.fill(200);
+    circleColor = p.random(255);
     reset();
   };
 
   /** フレームごとの描画処理 */
   p.draw = () => {
-    d += 2;
+    d += 4;
     if (d / 2 > p.dist(0, 0, p.width / 2, p.height / 2)) {
       reset();
     }
-    p.clear(0, 0, 0, 0);
+    p.background(bgColor);
+    p.fill(circleColor);
     p.circle(p.width / 2, p.height / 2, d);
   };
 
   const reset = () => {
     d = 0;
+    bgColor = circleColor;
+    circleColor = p.random(255);
   };
 };
 
