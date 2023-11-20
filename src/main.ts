9a6200e669a6de56c4a0c79904c3c12d8cb68d4c
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-11-20 20:47:15 +0900

    draw triangle

--- src/main.ts
+++ src/main.ts
@@ -2,51 +2,41 @@ import p5 from "p5";
 
 const sketch = (p: p5) => {
   /** 初期化処理 */
-  let x1: number, x2: number, x: number, y: number;
+  let route: { x: number; y: number }[], t: number, i: number;
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.textAlign(p.CENTER, p.CENTER);
+    route = [
+      { x: 100, y: 100 },
+      { x: 300, y: 300 },
+      { x: 300, y: 100 },
+    ];
 
-    x1 = 30;
-    x2 = p.width - 30;
-    x = x1;
-    y = p.height / 2;
+    t = 0;
+    i = 0;
   };
 
   /** フレームごとの描画処理 */
   p.draw = () => {
-    x++;
-    if (x >= x2) {
-      x = x1;
-    }
-
     p.clear(0, 0, 0, 0);
 
-    p.noStroke();
-    p.fill(240);
-    p.text(x1, x1, y - 65);
-    p.text(x2, x2, y - 65);
-    p.text(x, x, y - 65);
-    p.text(p.norm(x, x1, x2).toFixed(2), x, y + 65);
-    p.text(0, x1, y + 65);
-    p.text(1, x2, y + 65);
+    route.forEach((r) => {
+      p.circle(r.x, r.y, 20);
+    });
 
-    p.stroke(240);
-    p.noFill();
-    p.line(x1, y - 45, x1, y - 25);
-    p.line(x2, y - 45, x2, y - 25);
-    p.line(x, y - 45, x, y - 25);
-    p.line(x, y + 45, x, y + 25);
-    p.line(x1, y + 45, x1, y + 25);
-    p.line(x2, y + 45, x2, y + 25);
+    const prev = route[i];
+    const next = route[(i + 1) % route.length];
 
-    p.stroke(240);
-    p.noFill();
-    p.line(x1, y, x2, y);
+    const x = p.lerp(prev.x, next.x, t);
+    const y = p.lerp(prev.y, next.y, t);
 
-    p.stroke(240);
-    p.fill("#292a33");
-    p.circle(x, y, 20);
+    p.circle(x, y, 10);
+
+    t += 0.01;
+    if (t > 1) {
+      t = 0;
+      i++;
+      i %= route.length;
+    }
   };
 };
 
