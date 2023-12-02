8c574741531a6e0b32c8f426cfd5ae3dd2c9dcaa
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-12-02 22:33:33 +0900

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
+// import { defineConfig } from "vite";
+// import { resolve } from "path";
 
-const root = "src";
+// const root = "src";
+
+// export default defineConfig({
+//   root: root,
+//   build: {
+//     outDir: "../dist",
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, root, "index.html"),
+//         sample_01: resolve(__dirname, root, "sample_01/index.html"),
+//       },
+//     },
+//   },
+// });
+
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const root = resolve(__dirname, "src");
 
 export default defineConfig({
-  root: root,
-  build: {
-    outDir: "../dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, root, "index.html"),
-        privacy: resolve(__dirname, root, "privacy/index.html"),
-      },
-    },
-  },
+  root,
 });
