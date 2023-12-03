6b83c3d4fb7704d44dc943a1db8a01b32d87ddc3
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-12-03 11:23:08 +0900

    rotate

--- src/sample_01/main.ts
+++ src/sample_01/main.ts
@@ -14,17 +14,18 @@ const sketch = (p: p5) => {
     p.noFill();
     p.stroke(255);
 
-    for (let i = 0; i < 20; i++) {
-      const r = p.map(p.sin(p.frameCount), -1, 1, 0, 255);
-      const g = p.map(i, 0, 20, 0, 255);
-      const b = p.map(p.cos(p.frameCount), -1, 1, 255, 0);
+    for (let i = 0; i < 50; i++) {
+      const r = p.map(p.sin(p.frameCount), -1, 1, 100, 200);
+      const g = p.map(i, 0, 50, 100, 200);
+      const b = p.map(p.cos(p.frameCount), -1, 1, 200, 100);
       p.stroke(r, g, b);
+      p.rotate(p.frameCount / 20);
       p.beginShape();
-      for (let j = 0; j < 360; j += 10) {
-        const rad = i * 8;
+      for (let j = 0; j < 360; j += 60) {
+        const rad = i * 3;
         const x = rad * p.cos(j);
         const y = rad * p.sin(j);
-        const z = p.sin(p.frameCount * 2 + i * 10) * 50;
+        const z = p.sin(p.frameCount * 2 + i * 5) * 50;
         p.vertex(x, y, z);
       }
       p.endShape(p.CLOSE);
