037ef0e8a11007c43893ad899acad91ad80006eb
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-12-12 22:51:12 +0900

    create the colored sphere

--- src/sample_03/main.ts
+++ src/sample_03/main.ts
@@ -2,13 +2,31 @@ import p5 from "p5";
 
 const sketch = (p: p5) => {
   p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
     p.angleMode(p.DEGREES);
     p.background(30);
   };
 
   /** フレームごとの描画処理 */
-  p.draw = () => {};
+  p.draw = () => {
+    p.background(30);
+    p.noFill();
+    p.stroke(255);
+
+    for (let i = 0; i < 360; i++) {
+      p.push();
+
+      const r = p.map(p.sin(i), -1, 1, 200, 50);
+      const g = p.map(p.sin(i), -1, 1, 50, 200);
+      const b = p.map(p.cos(i), -1, 1, 50, 200);
+
+      p.stroke(r, g, b);
+      p.rotateY(i / 2);
+      p.ellipse(0, 0, 200);
+
+      p.pop();
+    }
+  };
 };
 
 new p5(sketch);
